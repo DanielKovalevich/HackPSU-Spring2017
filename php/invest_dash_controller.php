042fb679a81d5706d032b37db38a349cae2993fd
Unknown <dkoval526@gmail.com>
2017-04-08 17:10:13 -0400

    Finally got the API to work

--- php/invest_dash_controller.php
+++ php/invest_dash_controller.php
@@ -1,22 +1,23 @@
-<?php
-    require '/../vendor/autoload.php';
-    include("php/connection.php");
 
+<?php
+    require '../vendor/autoload.php';
+    include("php/connection.php");
     use GuzzleHttp\Client;
 
     if ($_SERVER["REQUEST_METHOD"] == "GET") {
-
         $client = new Client(['base_uri' => 'api.reimaginebanking.com']);
-        $array = $client->request('GET', 'accounts?key=f88bb319eadd8f435028df201925f4d');
-        $sql = "SELECT uid FROM WHERE username = " . $_SESSION["username"] . "";
+        $array = $client->get("http://api.reimaginebanking.com/accounts?key=f88bb319eadd8f435028df201925f4d2");
+        // $sql = "SELECT uid FROM WHERE username = " . $_SESSION["username"] . "";
+       // $id = $db->query($sql);
+       // $array = json_decode($array);
 
-        $id = $db->query($sql);
 
-        $array = json_decode($array);
+        $array = json_decode($array->getBody());
+        $array = $array[0];
+        print_r($array);
 
-        foreach($element as $array) {
-            print($element);
-        }
+        header("Location: http://104.236.109.78/invest_dashboard.php");
+        exit();
     }
 
 ?>
