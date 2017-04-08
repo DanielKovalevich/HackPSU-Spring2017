b50f29c77e28351ecd78d92afe16538131786b51
Daniel Kovalevich <dkoval526@gmail.com>
2017-04-08 14:21:20 -0400

    Test

--- php/invest_dash_controller.php
+++ php/invest_dash_controller.php
@@ -3,6 +3,9 @@
     include("curl.php");
     $curl = new Curl;
 
+    header("Location: http://104.236.109.78/invest_dashboard.php");
+    exit();
+
     if ($_SERVER["REQUEST_METHOD"] == "GET") {
         header("Location: http://104.236.109.78/invest_dashboard.php");
         exit();
