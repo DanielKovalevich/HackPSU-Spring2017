9df8b85874f981b2f6fa5c74cdff5323f54a6fa5
Unknown <dkoval526@gmail.com>
2017-04-08 20:34:41 -0400

    Vily didn't link to other pages

--- invest_dashboard.php
+++ invest_dashboard.php
@@ -62,7 +62,7 @@
                         </li>
                     </ul>
                      <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
-                    <div class="collapse navbar-collapse navbar-ex1-collapse">
+                         <div class="collapse navbar-collapse navbar-ex1-collapse">
                         <ul class="nav navbar-nav side-nav">
                             <li class="active">
                                 <a href="index.php"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
